884a5c24d26cfb04ecde0b5dd64ce6b6ffcf7167
caglaror <caglaror@gmail.com>
2019-05-05 18:32:21 -0700

    XMLHttpReuquest converted to async fetch api. popup.html first styled simple. Still under development.

--- js/background.js
+++ js/background.js
@@ -1,21 +1,29 @@
 //background worker
 //
+const targetURL = 'https://api.exchangeratesapi.io/latest?base=USD';
 chrome.browserAction.setBadgeText({text: '1sec'});
-trackRate();
-setInterval(trackRate,300000);
+//background worker
+//
 
-function trackRate(){
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET','https://api.exchangeratesapi.io/latest?base=USD');
-    //https://api.exchangeratesapi.io/latest?base=USD
-    xhr.send(null);
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            let dolarTL = Number(JSON.parse(xhr.responseText).rates.TRY);
-            dolarTL = Number.parseFloat(dolarTL).toFixed(2);
-            chrome.browserAction.setBadgeText({text: String(dolarTL)});
-        }
+class Xchange{
+    async static get(url){
+        const response = await fetch(url);
+        return await response.json(); //JSON objesi, return edildi
 
-        }
-
+    }
 }
+
+function currentCurrency(){
+    let request = new Xchange();
+    let usd2try = request.get(targetURL);
+    usd2try.then(datas => {
+        let dolarTL =  (Number.parseFloat(datas.rates.TRY).toFixed(2));
+
+        chrome.browserAction.setBadgeText({text: dolarTL});
+
+
+    })
+}
+
+currentCurrency();
+setInterval(currentCurrency,300000);
--- js/index.js
+++ js/index.js
@@ -1,18 +1,1 @@
-
-function apiCheck(){
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET','https://api.exchangeratesapi.io/latest?base=USD');
-    //https://api.exchangeratesapi.io/latest?base=USD
-    xhr.send(null);
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            let dolarTL = Number(JSON.parse(xhr.responseText).rates.TRY);
-            dolarTL = Number.parseFloat(dolarTL).toFixed(2);
-            document.querySelector('#dolarTL').textContent=dolarTL;
-        }
-
-    }
-
-}
-
-apiCheck();
+//*
